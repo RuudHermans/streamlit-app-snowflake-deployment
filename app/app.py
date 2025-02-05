f7faca03180eefb73dfb917163c19e3a90256100
Ruud84 <ruud1984.poc@gmail.com>
2025-02-05 12:31:46 +0100

    Added a new feature - Now also showing our newest customers

--- app/app.py
+++ app/app.py
@@ -10,6 +10,7 @@ USE_MOCK_DATA = os.getenv("USE_MOCK_DATA", "false").lower() == "true"
 if USE_MOCK_DATA:
     # Load mock data from CSV
     data = pd.read_csv(os.path.join(os.getcwd(), "mock_data", "mock_data_orders.csv"))
+    data2 = pd.read_csv(os.path.join(os.getcwd(), "mock_data", "customers.csv"))
 else:
     # Use Streamlit's Snowflake connection
     cnx = st.connection("snowflake")
@@ -17,6 +18,7 @@ else:
 
     # Fetch data from the orders table
     data = session.table("orders").select(col("order_number"), col("order_name")).to_pandas()
+    data2 = session.table("customers").select(col("customer_id"), col("customer_name"), col("email")).to_pandas()
 
 # Streamlit app
 st.title("Orders Dashboard")
@@ -24,3 +26,8 @@ st.write("Here are the latest orders:")
 
 # Display data
 st.dataframe(data)
+
+st.write("Here are the latest customers:")
+
+# Display data
+st.dataframe(data2)
